64b1791a851f9ab81bf3f54daa49ef89f495a4eb
Djole <djole@example.com>
2020-11-19 01:27:57 +1100

    inital commit

---
+++ equity.py
@@ -0,0 +1,54 @@
+import math
+
+chanceOfDoubles = 1/6
+chanceOfRegRoll = 1 - chanceOfDoubles
+movesPerTurn = 2
+
+winReward = 1
+lossReward = -1
+
+numPiecesOnLastFile = 4
+defaultState = {"blue": numPiecesOnLastFile, "white": numPiecesOnLastFile}
+
+maxTurns = math.ceil(numPiecesOnLastFile / movesPerTurn)
+
+class Turn:
+    toPlay = "blue"
+    state = defaultState
+
+    def __init__(self, toPlay, state):
+        self.toPlay = toPlay
+        self.state = state
+
+def evaluateTurnEquity(turn):
+    chanceOfWin = 0
+    if(turn.toPlay == "blue"):
+        if (turn.state["blue"] - movesPerTurn * 2) <= 0:
+            print("In 1")
+            chanceOfWin += chanceOfDoubles * winReward
+        else:
+            print("In 2")
+            chanceOfWin += chanceOfDoubles * evaluateTurnEquity(Turn("white", {"blue": turn.state["blue"] - movesPerTurn * 2, "white": turn.state["white"]}))
+        if (turn.state["blue"] - movesPerTurn) <= 0:
+            print("In 3")
+            chanceOfWin += chanceOfRegRoll * winReward
+        else: 
+            print("In 4")
+            chanceOfWin += chanceOfRegRoll * evaluateTurnEquity(Turn("white", {"blue": turn.state["blue"] - movesPerTurn, "white": turn.state["white"]}))
+    else:
+        if (turn.state["white"] - movesPerTurn * 2) <= 0:
+            print("In 5")
+            chanceOfWin += chanceOfDoubles * lossReward
+        else: 
+            print("In 6")
+            chanceOfWin += chanceOfDoubles * evaluateTurnEquity(Turn("blue", {"blue": turn.state["blue"], "white": turn.state["white"] - movesPerTurn * 2}))
+        if (turn.state["white"] - movesPerTurn <= 0):
+            print("In 7")
+            chanceOfWin += chanceOfRegRoll * lossReward
+        else: 
+            print("In 8")
+            chanceOfWin += chanceOfRegRoll * evaluateTurnEquity(Turn("blue", {"blue": turn.state["blue"], "white": turn.state["white"] - movesPerTurn}))
+    return chanceOfWin
+
+t = Turn("blue", defaultState)
+print("Blue raw equity: ", evaluateTurnEquity(t))           
